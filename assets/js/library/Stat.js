ff421c2ab1b32f7e9ec566ca887ce87f2e1b06cb
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2013-04-07 10:35:01 -0300

    Added code and tests for stats

--- assets/js/library/Stat.js
+++ assets/js/library/Stat.js
@@ -1,5 +1,5 @@
 d3.stat = (function($, _){
-    var data = {}, storage = null, DATA_KEY = 'stats';
+    var data = {}, storage = null, ts = null, DATA_KEY = 'stats';
 
     /**
      *
@@ -24,6 +24,7 @@ d3.stat = (function($, _){
                 storage = localStorage;
             }
         }
+
         return storage;
     }
 
@@ -32,7 +33,21 @@ d3.stat = (function($, _){
      * @return {Number}
      */
     function getTimestamp() {
-        return Math.round((new Date()).getTime() / 1000);
+        if (null === ts) {
+            ts = Math.round((new Date()).getTime() / 1000);
+        }
+
+        return ts;
+    }
+
+    /**
+     *
+     * @return {Number} newTs
+     */
+    function setTimestamp(newTs) {
+        ts = newTs;
+
+        return d3.stat;
     }
 
     /**
@@ -43,7 +58,18 @@ d3.stat = (function($, _){
         var jsonData;
 
         jsonData = getStorage().getItem(DATA_KEY);
-        data = $.decodeJSON(jsonData);
+        data = JSON.parse(jsonData);
+
+        return d3.stat;
+    }
+
+    /**
+     *
+     * @param {Object} newData
+     * @return {Object}
+     */
+    function setData(newData) {
+        data = newData;
 
         return d3.stat;
     }
@@ -53,7 +79,7 @@ d3.stat = (function($, _){
      * @return {Object}
      */
     function saveData() {
-        getStorage().setItem(DATA_KEY);
+        getStorage().setItem(DATA_KEY, JSON.stringify(data));
 
         return d3.stat;
     }
@@ -67,13 +93,17 @@ d3.stat = (function($, _){
     function saveResult(hashes, result) {
         var ts = getTimestamp();
 
-        result = parseInt(result==true);
+        result = result > 0 ? 1 : 0;
 
         _.each(hashes, function(hash){
             if (typeof data[hash] == 'undefined') {
                 data[hash] = [[result, ts]];
             } else {
-                data[hash].push([result, ts]);
+                data[hash].unshift([result, ts]);
+
+                if (data[hash].length > 10) {
+                    data[hash] = data[hash].slice(0, 10);
+                }
             }
         });
 
@@ -82,18 +112,67 @@ d3.stat = (function($, _){
 
     /**
      *
+     * @param {Object} hash
+     * @return {Number}
+     */
+    function getWordScore(hash) {
+        var score = 0, timePenalty = 100, ts = getTimestamp(), multiplier, word;
+
+        if (typeof data[hash] != 'undefined') {
+            word       = data[hash];
+            multiplier = _.size(word);
+
+            _.each(word, function(wordStat){
+                if (wordStat[0] == 1) {
+                    score += multiplier;
+                } else {
+                    score -= multiplier;
+                }
+            });
+
+            timePenalty = Math.floor((ts - word[word.length-1][1]) / 86400);
+            timePenalty = Math.min(timePenalty, 100);
+        }
+
+        return score - timePenalty;
+    }
+
+    /**
+     *
      * @param {Object} wordList
      * @return {Object}
      */
     function pickWord(wordList) {
+        var hashes = [], lowestScore = Number.MAX_VALUE, randomHash;
 
+        _.each(wordList, function(word, hash) {
+            var score = getWordScore(hash);
+
+            if (score == lowestScore) {
+                hashes.push(hash);
+            } else if (score < lowestScore) {
+                lowestScore = score;
+                hashes = [hash];
+            }
+        });
+
+        if (hashes.length == 0) {
+            return false;
+        }
+
+        randomHash = hashes[_.random(hashes.length-1)];
+
+        return wordList[randomHash];
     }
 
     loadData();
 
     return {
         setStorage: setStorage,
+        setData: setData,
+        setTimestamp: setTimestamp,
         saveResult: saveResult,
-        pickWord: pickWord
+        pickWord: pickWord,
+        DATA_KEY: DATA_KEY
     };
 })(jQuery, _);
