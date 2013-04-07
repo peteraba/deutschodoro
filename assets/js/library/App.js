c956a242d46266c042083aae1c8580b4b950d17f
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2013-04-07 12:03:39 -0300

    Minor refactorings

--- assets/js/library/App.js
+++ assets/js/library/App.js
@@ -1,17 +1,79 @@
 d3.app = (function($, _){
-    var importances, games;
+    var importances, games = null, dictionary = null, stat = null;
 
+    /**
+     *
+     * @return {Object}
+     */
     function getGames() {
-        if (!games) {
+        if (null === games) {
             games = d3.game;
         }
+
         return games;
     }
 
+    /**
+     *
+     * @param {Object} tmpGames
+     * @return {Object}
+     */
     function setGames(tmpGames) {
         games = tmpGames;
+
+        return d3.app;
     }
 
+    /**
+     *
+     * @return {Object}
+     */
+    function getDictionary() {
+        if (null === dictionary) {
+            dictionary = d3.dictionary;
+        }
+
+        return dictionary;
+    }
+
+    /**
+     *
+     * @param {Object} newDictionary
+     * @return {Object}
+     */
+    function setDictionary(newDictionary) {
+        dictionary = newDictionary;
+
+        return d3.app;
+    }
+
+    /**
+     *
+     * @return {Object}
+     */
+    function getStat() {
+        if (null === stat) {
+            stat = d3.stat;
+        }
+
+        return stat;
+    }
+
+    /**
+     *
+     * @param {Object} newStat
+     * @return {Object}
+     */
+    function setStat(newStat) {
+        stat = newStat;
+
+        return d3.app;
+    }
+
+    /**
+     *
+     * @return {Number}
+     */
     function getGamesSum() {
         var result;
         importances = {};
@@ -25,6 +87,10 @@ d3.app = (function($, _){
         return result;
     }
 
+    /**
+     *
+     * @return {Boolean|Object}
+     */
     function getRandomGame() {
         var sum, rand, game = null;
 
@@ -43,9 +109,25 @@ d3.app = (function($, _){
         if (game) {
             return d3.game[game];
         }
+
         return false;
     }
 
+    /**
+     *
+     * @param {Object} searchData
+     * @return {Object|Boolean}
+     */
+    function getWord(searchData) {
+        var wordList = getDictionary().findWords(searchData);
+
+        return wordList ? getStat().pickWord(wordList) : false;
+    }
+
+    /**
+     *
+     * @return {Boolean}
+     */
     function run() {
         var game = getRandomGame();
 
@@ -58,6 +140,9 @@ d3.app = (function($, _){
 
     return {
         run: run,
-        setGames: setGames
+        setGames: setGames,
+        setDictionary: setDictionary,
+        setStat: setStat,
+        getWord: getWord
     };
 })(jQuery, _);
--- test/library/AppTest.js
+++ test/library/AppTest.js
@@ -4,4 +4,36 @@ describe('app', function() {
             expect(d3.app.run()).to.be(true);
         });
     });
+
+    describe('#getWord()', function() {
+        var dict, statData;
+
+        dict = [
+            {level: 1, english: 'yes', german: 'ja', hash: 'a'},
+            {level: 1, english: 'no', german: 'nein', hash: 'b'},
+            {level: 2, english: 'similar', german: 'ähnlich', hash: 'c'},
+            {level: 3, english: 'enthusiastic', german: 'begeistert', hash: 'd'},
+            {level: 100, english: '', german: 'Vorsitz', hash: 'e'},
+        ];
+
+        statData = {
+            a: [[0, 0]],
+            b: [[1, 0]],
+            c: [[1, 0]],
+            d: [[1, 0]],
+            e: [[1, 0]]
+        };
+
+        beforeEach(function(){
+            d3.dictionary.setDictionary(dict);
+            d3.dictionary.setLevel(0);
+
+            d3.stat.setData(statData);
+        });
+
+        it('should return the word with the lowest score matching the search options.', function(){
+            expect(d3.app.getWord({})).to.be(dict[0]);
+            //expect(d3.app.getWord({english: 'yes'})).to.be(dict[1]);
+        });
+    });
 });
