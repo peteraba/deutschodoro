3744f2ee962ea08e0ad19e7d07d68833b50843c8
DevMonkCom <info@devmonk.com>
2013-04-17 22:12:27 +0200

    Minor fix for word to german spec

--- test/unit/library/game/wordToGerman.spec.js
+++ test/unit/library/game/wordToGerman.spec.js
@@ -8,16 +8,18 @@ define(
         context = createContext(stubs, _);
 
         context(['game/wordToGerman'], function (wordToGerman) {
-            describe('#checkResult()', function() {
-                it('should check if result is the correct German word', function(){
-                    expect(wordToGerman.checkResult()).to.equal(false);
+            describe('game.wordToGerman', function() {
+                describe('#checkResult()', function() {
+                    it('should check if result is the correct German word', function(){
+                        expect(wordToGerman.checkResult()).to.equal(false);
+                    });
                 });
-            });
 
-            describe('#getHtml()', function() {
-                it('should return html', function(){
-                    expect(wordToGerman.create()).to.equal(true);
-                    expect(wordToGerman.getHtml()).to.contain('<h1>');
+                describe('#getHtml()', function() {
+                    it('should return html', function(){
+                        expect(wordToGerman.create()).to.equal(true);
+                        expect(wordToGerman.getHtml()).to.contain('<h1>');
+                    });
                 });
             });
 
